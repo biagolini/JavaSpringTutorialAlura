c37be35cde18a1f4cf12a38ce165f7ef94e704e4
Carlos Biagolini-Jr <c.biagolini@gmail.com>
2022-01-19 11:54:31 -0300

    Parte 04  Endpoints

--- src/main/java/com/meuTutorial/forumDuvidas/controller/TopicosController.java
+++ src/main/java/com/meuTutorial/forumDuvidas/controller/TopicosController.java
@@ -1,20 +1,24 @@
 package com.meuTutorial.forumDuvidas.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.meuTutorial.forumDuvidas.controller.dto.TopicoDto;
-import com.meuTutorial.forumDuvidas.modelo.Curso;
 import com.meuTutorial.forumDuvidas.modelo.Topico;
+import com.meuTutorial.forumDuvidas.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
+
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
-    public List<TopicoDto> lista(){
-        Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programa-ção"));        
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+    public List<TopicoDto> lista() {
+        List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
     }
 }
