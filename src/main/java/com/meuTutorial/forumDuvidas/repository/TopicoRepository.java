430728645a4fc762a853faa39cd2df68ce481ea6
Carlos Biagolini-Jr <c.biagolini@gmail.com>
2022-01-19 13:20:50 -0300

    Parte 05  Filtro

--- src/main/java/com/meuTutorial/forumDuvidas/repository/TopicoRepository.java
+++ src/main/java/com/meuTutorial/forumDuvidas/repository/TopicoRepository.java
@@ -3,11 +3,19 @@ package com.meuTutorial.forumDuvidas.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.meuTutorial.forumDuvidas.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
-
 	List<Topico> findByCursoNome(String nomeCurso);
+
+	List<Topico> findByTitulo(String titulo);
+
+	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+	List<Topico> carregarPelaMinhaQueryPersonalizada(@Param("nomeCurso") String nomeCurso);
+
+
 
 }
