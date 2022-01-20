cde6a78c40bd1c120ec255f845efdf86edc2bcdc
Carlos Biagolini-Jr <c.biagolini@gmail.com>
2022-01-20 17:05:31 -0300

    Parte 11  Delete

--- src/main/java/com/meuTutorial/forumDuvidas/controller/TopicosController.java
+++ src/main/java/com/meuTutorial/forumDuvidas/controller/TopicosController.java
@@ -7,7 +7,9 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -70,5 +72,13 @@ public class TopicosController {
 		Topico topico = form.atualizar(id, topicoRepository);
 		return ResponseEntity.ok(new TopicoDto(topico));
 	}
+
+	// Metodo para excluir um registos
+    @DeleteMapping("/{id}")
+	@Transactional
+    public ResponseEntity remover(@PathVariable Long id) {
+        topicoRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body("Registro apagado com sucesso");
+    }
 
 }
