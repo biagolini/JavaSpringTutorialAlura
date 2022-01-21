270d96c7f1589670b2e8260c9edf9e4fe3422b38
Carlos Biagolini-Jr <c.biagolini@gmail.com>
2022-01-21 18:59:37 -0300

    Parte 18  Limpeza de Cache

--- src/main/java/com/meuTutorial/forumDuvidas/controller/TopicosController.java
+++ src/main/java/com/meuTutorial/forumDuvidas/controller/TopicosController.java
@@ -15,6 +15,7 @@ import com.meuTutorial.forumDuvidas.repository.CursoRepository;
 import com.meuTutorial.forumDuvidas.repository.TopicoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -58,6 +59,7 @@ public class TopicosController {
 
 	// Metodo para postar um topico
 	@PostMapping
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody  @Valid  TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -79,6 +81,7 @@ public class TopicosController {
 	// Metodo para atualizar dados
 	@PutMapping("/{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if (optional.isPresent()) {
@@ -93,6 +96,7 @@ public class TopicosController {
 	// Metodo para excluir um registos
 	@DeleteMapping("/{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<?> remover(@PathVariable Long id) {
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if (optional.isPresent()) {
