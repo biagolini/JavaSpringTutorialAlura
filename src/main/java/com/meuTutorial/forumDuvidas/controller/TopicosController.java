46fbbc4c91dbfbdc0581ddb45a91a26700070b61
Carlos Biagolini-Jr <c.biagolini@gmail.com>
2022-01-21 14:12:05 -0300

    Parte 14  Ordenção com Spring Data WebSupport

--- src/main/java/com/meuTutorial/forumDuvidas/controller/TopicosController.java
+++ src/main/java/com/meuTutorial/forumDuvidas/controller/TopicosController.java
@@ -16,7 +16,6 @@ import com.meuTutorial.forumDuvidas.repository.TopicoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
