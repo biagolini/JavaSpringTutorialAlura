23f681184d65ef7d14d6553b2744fbda2faf78ee
Carlos Biagolini-Jr <c.biagolini@gmail.com>
2022-01-21 18:10:07 -0300

    Paginas index + 404

--- src/main/java/com/meuTutorial/forumDuvidas/controller/TopicosController.java
+++ src/main/java/com/meuTutorial/forumDuvidas/controller/TopicosController.java
@@ -15,6 +15,7 @@ import com.meuTutorial.forumDuvidas.repository.CursoRepository;
 import com.meuTutorial.forumDuvidas.repository.TopicoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
@@ -43,6 +44,7 @@ public class TopicosController {
 	
 	// Lista de todos os topicos com paginação
 	@GetMapping
+	@Cacheable(value = "listaDeTopicos")
 	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @PageableDefault(sort = "dataCriacao", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao) {
 		
 		if (nomeCurso == null) {
